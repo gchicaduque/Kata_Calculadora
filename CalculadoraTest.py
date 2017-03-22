22a35bad2fb4424f847f634ddcae2b90d7465521
GERMAN <gchicaduque@gmail.com>
2017-03-22 12:25:02 -0500

    Paso 2-Se agrega test para probar con un numero (Rojo)

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -7,3 +7,6 @@ __author__ = 'German Chica'
 class CalculadoraTest(TestCase):
     def test_sumar(self):
         self.assertEqual(Calculadora().sumar(""),0,"Cadena vacia")
+
+    def test_sumar_unacadena(self):
+        self.assertEqual(Calculadora().sumar("1"),1,"Un numero")
